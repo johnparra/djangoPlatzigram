8d559eaf4d57c7508307a765bce688a3eab120a8
John Parra <ingsis.johnparra@gmail.com>
2021-11-11 17:16:58 -0500

    1st model created

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    email = models.EmailField()
+    password: str = models.CharField(max_length=8)
+
+    first_name: str = models.CharField(max_length=20)
+    last_name: str = models.CharField(max_length=20)
+
+    bio = models.TextField()
+
+    birthdate = models.DateField()
+
+    created = models.DateField()
+    modified = models.DateField()
